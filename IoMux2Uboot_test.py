7e6692838a693eef645927de84a7e9945f512e07
vlad <vlad@v-lad.org>
2014-08-01 19:32:18 -0400

    Fixed up tests

--- IoMux2Uboot_test.py
+++ IoMux2Uboot_test.py
@@ -2,11 +2,22 @@
 """
 Unit tests for conversion script
 """
-import config
 
 __author__ = 'vlad'
 
 from unittest import TestCase
-from Dataset import *
+from IoMux2Uboot import *
+
+
+
+
+class DatasetTest(TestCase):
+
+    def setUp(self):
+        pass
+
+    def test_number_of_users(self):
+
+        self.assertGreater(90, 11)
 
 
