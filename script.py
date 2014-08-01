c6c205074fa004e4f162f301ba23ff6e9cac3f2c
Ashish Singh <gurashishs@gmail.com>
2014-08-01 16:58:59 -0400

    Get info from IoMux to identify pad.

--- script.py
+++ script.py
@@ -1,12 +1,20 @@
 import xml.etree.ElementTree as ET
+
 w = open('DCD_commands.c', 'w');
 
-tree = ET.parse('sample.xml');
+# Get information from IoMux XML file 
+tree = ET.parse('samples/i.MX6SDL_Sabre_AI_RevA.IomuxDesign.xml');
 root = tree.getroot();
+addresses = []; names = []; nets = []; alts = [];
 
-for signal in root.findall(".//SignalDesign[@Name]"):
-	if 'I2C' in signal.get('Name'):
-		w.write( '// %s Ball: %s\n' % (signal.get('Name'), signal.find(".//Routing").get('ball')) )
-		for register in signal.findall(".//Register"):
-			w.write( '// %s\n' % (register.get('Name')) )
-			w.write( ':write %s,%s\n' % (register.get('Address'), register.get('Value')) )
+for signal in root.findall(".//SignalDesign[@IsChecked='true']"):
+	for register in signal.findall(".//Register"):
+		if 'SW_PAD_CTL_PAD' in register.get('Name') and 'ALT' in signal.find(".//Routing").get('mode'):
+			addresses.append(register.get('Address')[6:])
+			names.append(signal.get('Name'))
+			nets.append(signal.find(".//Routing").get('padNet')[7:])
+			alts.append(signal.find(".//Routing").get('mode')[-1:])
+
+w = open('iomux', 'w');
+[w.write('Name: %16s, Net: %16s, Mode: %s @ %s\n' % (names[i], nets[i], alts[i], addresses[i])) for i in range(0, len(addresses))];
+w.close();
