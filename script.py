0b9a140405408c3ea8809e3c8487902d34df68df
Ashish Singh <gurashishs@gmail.com>
2014-08-01 17:36:47 -0400

    C syntax translation without grouping

--- script.py
+++ script.py
@@ -40,3 +40,14 @@ if DEBUG:
 	w = open('pinDict', 'w');
 	[w.write('%s: %s\n' % (pin, pinDict[pin])) for pin in pinDict];
 	w.close();
+
+# Write pad setup and comment to file
+w = open('results', 'w');
+
+for i in range(0, len(addresses)):
+	pins = pinDict[addresses[i]];
+	mode = int(alts[i])
+	comment = names[i] + "--" + nets[i]
+	w.write('mxc_iomux_v3_setup_pad(%s) // %s\n' % (pins[mode], comment));
+
+w.close();
