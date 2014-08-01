b6158911e10961d1c30082d186b9d816d089df2a
vlad <vlad@v-lad.org>
2014-08-01 19:48:17 -0400

    Formatting change.  Tabs are bad

--- IoMux2Uboot.py
+++ IoMux2Uboot.py
@@ -2,7 +2,7 @@
 # This is so that the pads can be grouped by instance as in the IOMux.
 # The goal was to write smaller functions like setup_eim(), setup_gpio1(), etc.
 
-# Created the dictionay. Just need to change the matching functionality to adapt 
+# Created the dictionary. Just need to change the matching functionality to adapt
 # the new data structure. Also need to create the function creation.
 
 import xml.etree.ElementTree as ET
@@ -15,48 +15,49 @@ root = tree.getroot()
 padDict = dict()
 
 for signal in root.findall(".//SignalDesign[@IsChecked='true']"):
-	for register in signal.findall(".//Register"):
-		if 'SW_PAD_CTL_PAD' in register.get('Name') and 'ALT' in signal.find(".//Routing").get('mode'):
-			address = register.get('Address')[6:]
-			name = signal.get('Name')
-			net = signal.find(".//Routing").get('padNet')[7:]
-			mode = signal.find(".//Routing").get('mode')[-1:]
-			instance = signal.get('Instance')
+    for register in signal.findall(".//Register"):
+        if 'SW_PAD_CTL_PAD' in register.get('Name') and 'ALT' in signal.find(".//Routing").get('mode'):
+            address = register.get('Address')[6:]
+            name = signal.get('Name')
+            net = signal.find(".//Routing").get('padNet')[7:]
+            mode = signal.find(".//Routing").get('mode')[-1:]
+            instance = signal.get('Instance')
 
-			try:
-				padDict[instance][address] = [name, net, mode]
-			except KeyError:
-				padDict[instance] = dict()
-				padDict[instance][address] = [name, net, mode]
+            try:
+                padDict[instance][address] = [name, net, mode]
+            except KeyError:
+                padDict[instance] = dict()
+                padDict[instance][address] = [name, net, mode]
 
 if DEBUG:
-	w = open('iomux', 'w')
-	for instance in padDict:
-		for address in padDict[instance]:
-			pad = padDict[instance][address]
-			w.write('Instance: %8s, Address: @ %s, Name: %20s, Net: %16s, Mode: %1s\n' % (instance, address, pad[0], pad[1], pad[2]))
-	w.close();
+    w = open('iomux', 'w')
+    for instance in padDict:
+        for address in padDict[instance]:
+            pad = padDict[instance][address]
+            w.write('Instance: %8s, Address: @ %s, Name: %20s, Net: %16s, Mode: %1s\n' % (
+                instance, address, pad[0], pad[1], pad[2]))
+    w.close();
 
 # Create nested dictionary from header file.
 pins = open('headers/mx6dl_pins.h').readlines()
 pinDict = dict()
 
 for i in range(0, len(pins)):
-	if pins[i].startswith('#define MX6DL_PAD'):
-		name = pins[i][8:].split()[0]
-		addr = pins[i+1].strip()[12:].split(',')[0]
-		mode = int(pins[i+1].strip()[12:].split(',')[2].strip().split('|')[0].strip())
+    if pins[i].startswith('#define MX6DL_PAD'):
+        name = pins[i][8:].split()[0]
+        addr = pins[i + 1].strip()[12:].split(',')[0]
+        mode = int(pins[i + 1].strip()[12:].split(',')[2].strip().split('|')[0].strip())
 
-		try:
-			pinDict[addr][mode] = name
-		except KeyError:
-			pinDict[addr] = dict()
-			pinDict[addr][mode] = name
+        try:
+            pinDict[addr][mode] = name
+        except KeyError:
+            pinDict[addr] = dict()
+            pinDict[addr][mode] = name
 
 if DEBUG:
-	w = open('pinDict', 'w')
-	[w.write('%s: %s\n' % (pin, pinDict[pin])) for pin in pinDict]
-	w.close()
+    w = open('pinDict', 'w')
+    [w.write('%s: %s\n' % (pin, pinDict[pin])) for pin in pinDict]
+    w.close()
 
 # Write pad setup and comment to file
 w = open('results', 'w');
