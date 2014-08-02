40420556fa2c34eeef18760136190feb735498d4
vlad <vlad@v-lad.org>
2014-08-02 14:58:54 -0400

    Added test for write pin dict

--- IoMux2Uboot_test.py
+++ IoMux2Uboot_test.py
@@ -6,30 +6,44 @@ Unit tests for conversion script
 __author__ = 'vlad'
 
 from unittest import TestCase
-from mock import Mock
+from mock import patch, call
+from mock import mock_open
 
 from IoMux2Uboot import *
 
 
+class DebugTest(TestCase):
+
+    def setUp(self):
+        pass
+
+    def test_write_dict(self):
+
+        m = mock_open()
+        with patch('IoMux2Uboot.open', m, create=True):
+            dump_pin_dict({1: {1: "BOB", 2: "BOLL"}, 2: {2: "JANE", 314: "PETE"}})
+
+        m.assert_called_once_with("pinDict", "w")
+        h = m()
+
+        calls = [call.write("1: {1: 'BOB', 2: 'BOLL'}\n"), call().write("2: {2: 'JANE', 314: 'PETE'}\n")]
+        h.write.assert_has_calls(calls)
 
 
-class DatasetTest(TestCase):
+class ParserTest(TestCase):
 
     def setUp(self):
         self.root = parse_iomux("samples/i.MX6SDL_Sabre_AI_RevA.IomuxDesign.xml")
         self.sig_enet_mdio = self.root.findall(".//SignalDesign[@Name='ENET_MDIO']")
-        self.all_signals =  self.root.findall(".//SignalDesign[@IsChecked='true']")
-
-
+        self.all_signals = self.root.findall(".//SignalDesign[@IsChecked='true']")
 
     def test_must_be_330_checked_signals(self):
-        checked_signals =  self.root.findall(".//SignalDesign[@IsChecked='true']")
-        self.assertEqual(len(checked_signals),330)
+        checked_signals = self.root.findall(".//SignalDesign[@IsChecked='true']")
+        self.assertEqual(len(checked_signals), 330)
 
     def test_must_be_330_total_signals(self):
 
-        self.assertEqual(len(self.all_signals),330)
-
+        self.assertEqual(len(self.all_signals), 330)
 
     def test_how_many_signals_have_alt_routing(self):
 
