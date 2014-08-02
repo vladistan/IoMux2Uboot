27fda5e356facacc049d78fc731b4b35284b95ab
vlad <vlad@v-lad.org>
2014-08-02 08:46:09 -0400

    Added some assertions regarding number of signals without routing

--- IoMux2Uboot_test.py
+++ IoMux2Uboot_test.py
@@ -18,6 +18,8 @@ class DatasetTest(TestCase):
     def setUp(self):
         self.root = parse_iomux("samples/i.MX6SDL_Sabre_AI_RevA.IomuxDesign.xml")
         self.sig_enet_mdio = self.root.findall(".//SignalDesign[@Name='ENET_MDIO']")
+        self.all_signals =  self.root.findall(".//SignalDesign[@IsChecked='true']")
+
 
 
     def test_must_be_330_checked_signals(self):
@@ -26,8 +28,31 @@ class DatasetTest(TestCase):
 
     def test_must_be_330_total_signals(self):
 
-        all_signals =  self.root.findall(".//SignalDesign[@IsChecked='true']")
-        self.assertEqual(len(all_signals),330)
+        self.assertEqual(len(self.all_signals),330)
+
+
+    def test_how_many_signals_have_alt_routing(self):
+
+        rt = self.root.findall("./SignalDesign/Routing")
+
+        self.assertEquals(len(rt),330)
+
+        # There should be 117 signal w/o routing
+        no_alt = [x.get('padNet') for x in rt if not "ALT" in x.get("mode")  ]
+        self.assertEquals(len(no_alt),117)
+
+
+        # There should be 111 signal w/o routing that belong to DRAM
+        no_alt_wo_dram = [x for x in no_alt if not 'DRAM' in x ]
+        self.assertEquals(len(no_alt_wo_dram),6)
+
+        # There should be 6 signal w/o routing that belong to JTAG
+        no_alt_wo_jtag = [x for x in no_alt_wo_dram if not 'JTAG' in x ]
+        self.assertEquals(len(no_alt_wo_jtag),0)
+
+        # There should be 0 signal w/o routing that belong neither to DRAM nor JTAG
+
+
 
 
 
